75110f9bd649318ab5de3985c62d8ab31ffdde30
Vinícius Cavalcanti <43768274+hvini@users.noreply.github.com>
2024-09-04 21:26:08 -0300

    update keymap base layer

--- keyboards/bastardkb/charybdis/3x5/keymaps/hvini/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/hvini/keymap.c
@@ -76,9 +76,9 @@ combo_t key_combos[] = {
 // clang-format off
 /** \brief QWERTY layout (3 rows, 10 columns). */
 #define LAYOUT_LAYER_BASE                                                                     \
-       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, \
-       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_QUOT, \
-       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, \
+       KC_QUOT,    KC_COMM,    KC_DOT,    KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L, \
+       KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N, KC_S, \
+       KC_SCLN,    KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M, KC_W,  KC_V, KC_Z, \
                       ESC_MED, SPC_NAV, TAB_FUN, ENT_SYM, BSP_NUM
 
 /** Convenience row shorthands. */
